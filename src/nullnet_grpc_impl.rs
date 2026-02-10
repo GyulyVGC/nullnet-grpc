53e0e5124a404594f49872217d86c0198f2859dc
GyulyVGC <gyulyvgc99@gmail.com>
2026-02-10 17:11:30 +0100

    create all VLANs of a chain in parallel

--- src/nullnet_grpc_impl.rs
+++ src/nullnet_grpc_impl.rs
@@ -196,81 +196,94 @@ impl NullnetGrpcImpl {
         &self,
         dep_chain: Vec<((IpAddr, Client), (IpAddr, Client))>,
     ) -> Result<IpAddr, Error> {
-        let mut upstream_ip = IpAddr::V4(Ipv4Addr::UNSPECIFIED);
-
+        let mut join_set_outer = JoinSet::new();
         for ((client_ethernet, client), (server_ethernet, server)) in dep_chain {
-            let init_time = std::time::Instant::now();
+            let services = self.services.clone();
+            let orchestrator = self.orchestrator.clone();
+            let last_registered_vlan = self.last_registered_vlan.clone();
+            join_set_outer.spawn(async move {
+                let init_time = std::time::Instant::now();
 
-            // check if the link is already set up
-            let server_service = self.services.read().await.get(&server.name()).cloned();
-            if let Some(ServiceInfo::Registered(reg)) = server_service
-                && reg.is_client_setup(&client).is_some()
-            {
-                continue;
-            }
+                // check if the link is already set up
+                let server_service = services.read().await.get(&server.name()).cloned();
+                if let Some(ServiceInfo::Registered(reg)) = server_service
+                    && reg.is_client_setup(&client).is_some()
+                {
+                    return None;
+                }
 
-            let vlan_id = self.next_vlan_id().await;
+                let mut last_id = last_registered_vlan.lock().await;
+                *last_id += 1;
+                let vlan_id = *last_id;
+                drop(last_id);
 
-            let destinations = [client_ethernet, server_ethernet];
-            // remove duplicates from destinations (in case services are hosted on the same machine)
-            let destinations: HashSet<IpAddr> = destinations.iter().copied().collect();
+                let [a, b] = vlan_id.to_be_bytes();
+                let server_veth = IpAddr::V4(Ipv4Addr::new(10, a, b, 1));
+                let client_veth = IpAddr::V4(Ipv4Addr::new(10, a, b, 2));
+                let host_mapping = Some(HostMapping {
+                    ip: server_veth.to_string(),
+                    name: server.name(),
+                });
+                let msg = VlanSetup {
+                    msg_id: None,
+                    client_ethernet: client_ethernet.to_string(),
+                    server_ethernet: server_ethernet.to_string(),
+                    client_veth: client_veth.to_string(),
+                    server_veth: server_veth.to_string(),
+                    vlan_id: u32::from(vlan_id),
+                    host_mapping,
+                };
 
-            let [a, b] = vlan_id.to_be_bytes();
-            let server_veth = IpAddr::V4(Ipv4Addr::new(10, a, b, 1));
-            let client_veth = IpAddr::V4(Ipv4Addr::new(10, a, b, 2));
-            if client.is_proxy() {
-                upstream_ip = server_veth;
-            }
+                let upstream_ip = if client.is_proxy() {
+                    Some(server_veth)
+                } else {
+                    None
+                };
 
-            let host_mapping = Some(HostMapping {
-                ip: server_veth.to_string(),
-                name: server.name(),
+                let destinations = [client_ethernet, server_ethernet];
+                // remove duplicates from destinations (in case services are hosted on the same machine)
+                let destinations: HashSet<IpAddr> = destinations.iter().copied().collect();
+
+                let mut join_set_inner = JoinSet::new();
+                for dest in destinations {
+                    let msg = msg.clone();
+                    let orchestrator = orchestrator.clone();
+                    join_set_inner.spawn(async move {
+                        // TODO: handle errors?
+                        let _ = orchestrator.send_vlan_setup(dest, msg).await;
+                        println!("{dest} acknowledged");
+                    });
+                }
+
+                while join_set_inner.join_next().await.is_some() {}
+
+                // register the link between the two services
+                services
+                    .write()
+                    .await
+                    .entry(server.name())
+                    .and_modify(|si| {
+                        if let ServiceInfo::Registered(reg) = si {
+                            let time_ms = init_time.elapsed().as_millis();
+                            let ci = ClientInfo::new(client_veth, server_veth, vlan_id, time_ms);
+                            reg.add_client(client, ci);
+                        }
+                    });
+
+                upstream_ip
             });
-
-            let msg = VlanSetup {
-                msg_id: None,
-                client_ethernet: client_ethernet.to_string(),
-                server_ethernet: server_ethernet.to_string(),
-                client_veth: client_veth.to_string(),
-                server_veth: server_veth.to_string(),
-                vlan_id: u32::from(vlan_id),
-                host_mapping,
-            };
-
-            let mut join_set = JoinSet::new();
-            for dest in destinations {
-                let msg = msg.clone();
-                let orchestrator = self.orchestrator.clone();
-                join_set.spawn(async move {
-                    // TODO: handle errors?
-                    let _ = orchestrator.send_vlan_setup(dest, msg).await;
-                    println!("{dest} acknowledged");
-                });
-            }
-
-            while join_set.join_next().await.is_some() {}
-
-            // register the link between the two services
-            self.services
-                .write()
-                .await
-                .entry(server.name())
-                .and_modify(|si| {
-                    if let ServiceInfo::Registered(reg) = si {
-                        let time_ms = init_time.elapsed().as_millis();
-                        let ci = ClientInfo::new(client_veth, server_veth, vlan_id, time_ms);
-                        reg.add_client(client, ci);
-                    }
-                });
         }
 
-        Ok(upstream_ip)
-    }
+        let mut ret_val = None;
+        while let Some(res) = join_set_outer.join_next().await {
+            if let Ok(Some(upstream_ip)) = res {
+                ret_val = Some(upstream_ip);
+            }
+        }
 
-    async fn next_vlan_id(&self) -> u16 {
-        let mut last_id = self.last_registered_vlan.lock().await;
-        *last_id += 1;
-        *last_id
+        ret_val
+            .ok_or("No valid upstream IP found after VLAN chain setup")
+            .handle_err(location!())
     }
 
     async fn generate_graphviz(&self) -> Result<(), Error> {
