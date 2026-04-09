e1eb66fdf259f5b69f9e3f1a169fe00ae3824bc1
GyulyVGC <gyulyvgc99@gmail.com>
2026-04-09 12:12:06 +0200

    write logs to file

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ mod timeout;
 use crate::nullnet_grpc_impl::NullnetGrpcImpl;
 use crate::proto::nullnet_grpc::nullnet_grpc_server::NullnetGrpcServer;
 use nullnet_liberror::{Error, ErrorHandler, Location, location};
+use std::fs::create_dir_all;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::{panic, process};
 use tonic::transport::Server;
@@ -21,6 +22,15 @@ const PORT: u16 = 50051;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let _gag1: gag::Redirect<std::fs::File>;
+    let _gag2: gag::Redirect<std::fs::File>;
+    if let Some((gag1, gag2)) = redirect_stdout_stderr_to_file() {
+        _gag1 = gag1;
+        _gag2 = gag2;
+    } else {
+        println!("Failed to redirect stdout and stderr to file, logs will be printed to console");
+    }
+
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), PORT);
 
     let mut server = Server::builder();
@@ -55,4 +65,24 @@ async fn init_nullnet() -> Result<NullnetGrpcImpl, Error> {
     .handle_err(location!())?;
 
     NullnetGrpcImpl::new().await
+}
+
+fn redirect_stdout_stderr_to_file()
+-> Option<(gag::Redirect<std::fs::File>, gag::Redirect<std::fs::File>)> {
+    let dir = "/var/log/nullnet";
+    create_dir_all(dir).handle_err(location!()).ok()?;
+    let timestamp = chrono::Utc::now().format("%Y-%m-%d_%H-%M-%S");
+    let file_path = format!("{dir}/grpc_{timestamp}.txt");
+    if let Ok(logs_file) = std::fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&file_path)
+    {
+        println!("Writing logs to '{file_path}'");
+        return Some((
+            gag::Redirect::stdout(logs_file.try_clone().ok()?).ok()?,
+            gag::Redirect::stderr(logs_file).ok()?,
+        ));
+    }
+    None
 }
